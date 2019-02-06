9bab05c347b2a31f20a5161789147903be3b519d
ankushatgithub <g.ankushvit09@gmail.com>
2019-02-06 18:06:30 +0530

    Program Updated

--- Guess.py
+++ Guess.py
@@ -7,11 +7,13 @@ print (RandomNum)
 times = 0
 InputNum = int(input("Enter the random number between 1 to 100: "))
 times += 1
+if RandomNum == InputNum:
+    print("Congrats !!!! Your guessed it right in 1st attempt :)")
 while RandomNum != InputNum:
-
-    if RandomNum == InputNum:
-        print("Congrats !!!! Your guessed it right in %s time :)" %times)
-    elif RandomNum < InputNum:
+    if times >= 3:
+        print("Sorry !!! Number of attempts exceeded ...")
+        break
+    if RandomNum < InputNum:
         print("oops !! that was not quite right. Try little lower number...")
         InputNum = int(input("\nEnter the Next random number you have guessed: "))
         times += 1
@@ -19,5 +21,6 @@ while RandomNum != InputNum:
         print("oops !! that was not quite right. Try little higher number...")
         InputNum = int(input("\nEnter the Next random number you have guessed: "))
         times += 1
-    if times > 3:
+    if RandomNum == InputNum:
+        print("Congrats !!!! Your guessed it right in %s attempts :)" % times)
         break
