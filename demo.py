4fdc03a4c89e456f411cdb15609d826d5635038e
ankushatgithub <g.ankushvit09@gmail.com>
2019-02-06 14:16:53 +0530

    Program Added

--- demo.py
+++ demo.py
@@ -1,1 +1,23 @@
-print("Hello There")
+class A():
+    def func(self):
+        print("In A Func")
+
+    def func2(self):
+        print("In A Func2")
+
+
+class B():
+    def func(self):
+        print("B In Func")
+
+    def func2(self):
+        print("B In Func2")
+
+
+class C(B,A):
+    def func(self):
+        print("C In Func2")
+
+
+a = C()
+a.func2()
